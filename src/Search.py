51787e61e41f17aeae5d522df087ea7a38cbc9e2
guwonstan <129030804+guwonstan@users.noreply.github.com>
2023-10-19 22:38:34 +0800

    best first search bug fixes

--- src/Search.py
+++ src/Search.py
@@ -1,19 +1,25 @@
 import heapq
 
+#Make sure to use GRID_SIZE_DEF+2 for the size parameter
 def BestFirstSearch(WallTable, size):
     # Tuple Format: (h(n), x, y, parent_x, parent_y)
-    priority_heap = [((size - 1) * 2, 1, 1, None, None)]
+    priority_heap = [((size - 3) * 2, 1, 1, None, None)]
     heapq.heapify(priority_heap)
-    shortest_path = []
+    parent = [[None, None] * (size + 2) for _ in range(size + 2)]
     visited = {(1, 1)}
 
     while priority_heap:
         current = heapq.heappop(priority_heap)
         row_index, col_index = current[1], current[2]
         parent_row, parent_col = current[3], current[4]
-        shortest_path.append((row_index, col_index, parent_row, parent_col))
+        parent[row_index][col_index] = [parent_row, parent_col]
 
         if row_index == size-2 and col_index == size-2:
+            shortest_path = []
+            node =  [size-2, size-2]
+            while node[0] != None:
+                shortest_path.append(node)
+                node = parent[node[0]][node[1]]
             return shortest_path
 
         # Check each adjacent cell if they are walls, if not push to heap alongside h(n)
@@ -21,17 +27,10 @@ def BestFirstSearch(WallTable, size):
         for i, j in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
             new_row, new_col = row_index + i, col_index + j
             if (WallTable[new_row][new_col] == 0) and ((new_row, new_col) not in visited):
-                manhattan = (size - new_row) + (size - new_col)
-                valid_moves.append((manhattan, new_row, new_col, row_index, col_index))
+                manhattan = (size - new_row - 2) + (size - new_col - 2)
+                heapq.heappush(priority_heap, (manhattan, new_row, new_col, row_index, col_index))
                 visited.add((new_row, new_col))
-
-        if not valid_moves:
-            parent_row, parent_col = shortest_path[-1][2], shortest_path[-1][3]
-            shortest_path.pop()
-        else:
-            for element in valid_moves:
-                heapq.heappush(priority_heap, element)
-
+    
     return None
 
 WallTable = [
