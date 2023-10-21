47783c7efd8ec7afc09ece154d2af0bb1db93a8f
lettertoelias <iyahcapuchinom@gmail.com>
2023-10-21 21:38:51 +0800

    added button 1 rewind function

--- src/gui/mazestep_frame.py
+++ src/gui/mazestep_frame.py
@@ -2,10 +2,10 @@ from tkinter import ttk
 
 
 class MazeStepframe(ttk.LabelFrame):
-    def __init__(self, parent, mazeAlgoFrame, *args, **kwargs):
+    def __init__(self, parent, mazeFrame, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
         self.parent = parent
-        self.mazeAlgoFrame = mazeAlgoFrame
+        self.mazeFrame = mazeFrame
         self.stepsLabel = None
         self.stepsBox = None
         self.buttonFrame = None
@@ -30,7 +30,7 @@ class MazeStepframe(ttk.LabelFrame):
         self.buttonFrame = ttk.Frame(self)
         self.buttonFrame.grid(row=1, column=1, columnspan=1, padx=10, pady=5, sticky="w")
 
-        self.button1 = ttk.Button(self.buttonFrame, text="⏮", width=3)
+        self.button1 = ttk.Button(self.buttonFrame, text="⏮", width=3, command=self.button1_command)
         self.button2 = ttk.Button(self.buttonFrame, text="⏴", width=3)
         self.button3 = ttk.Button(self.buttonFrame, text="⏵", width=3)
         self.button4 = ttk.Button(self.buttonFrame, text="⏭", width=3)
@@ -39,3 +39,22 @@ class MazeStepframe(ttk.LabelFrame):
         self.button2.grid(row=0, column=1, padx=(0,5))
         self.button3.grid(row=0, column=2, padx=(0,5))
         self.button4.grid(row=0, column=3, padx=(0,5))
+        
+    def button1_command(self):
+        wallTable = self.mazeFrame.wallTable
+        playableSize = self.mazeFrame.playableSize
+        allMaze = [(i, j, wallTable[i][j]) for i in range(playableSize+2) for j in range(playableSize+2)]
+        self.mazeFrame.update_cells(allMaze, bg="white")
+        self.mazeFrame.update_source_target()
+        pass
+    
+    '''
+    def button2_command(self):
+        pass
+    
+    def button3_commdand(self):
+        pass
+    
+    def button4_command(self):
+        pass
+    '''
